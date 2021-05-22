b93d6c62d247a74abcd617e34defca7a0e0fe45a
Slin Castro <scastro@thoughtworks.com>
2021-05-21 19:27:25 -0500

    include pandas in the tweet processor

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # TweetsSentimentAnalisis
+
+* Create Virtual environment :
+   ` python3 -m venv .`
+* Run Virtual environment :
+    `source bin/activate`
+
+* Install Pandas : 
+    `pip install pandas` 
+    `pip install matplotlib`
--- SentimentAnalisis.py
+++ SentimentAnalisis.py
@@ -1,8 +1,9 @@
 import json
+import pandas as pd
+import matplotlib.pyplot as plot
 
-sentimentRecords = open("Sentimientos.txt")
-tweetRecordsFile = "Tweets.txt"
-values = {}
+sentiment_records = open("Sentimientos.txt")
+tweet_records_file = "Tweets.txt"
 
 def get_tweets_text(tweet_records_file):
     tweetRecords = open(tweet_records_file)
@@ -11,27 +12,57 @@ def get_tweets_text(tweet_records_file):
 
     for tweet in tweetRecords:
         data = json.loads(tweet)
-        if data.has_key(key_to_search) :
+        if key_to_search in data :
             tweetText = data[key_to_search]
             text_tweets_to_process.append(tweetText)
 
     return text_tweets_to_process
 
-for record in sentimentRecords:
-    sentiment, value = record.split("\t")
-    values[sentiment] = int(value)
+def calculate_tweets_value(tweets, sentiments_values):
+    sentiment_data = []
+    for tweet in tweets:
+        words_by_tweet = tweet.split()
+        associate_sentiment_value = 0
+        for word in words_by_tweet:
+            if word in values:
+                sentimentNumber = sentiments_values[word]
+                associate_sentiment_value = associate_sentiment_value + sentimentNumber
 
-tweets = get_tweets_text(tweetRecordsFile)
+        sentiment_data.append([tweet, associate_sentiment_value])
 
-for tweet in tweets:
-    words_by_tweet = tweet.split()
-    value = 0
-    for word in words_by_tweet:
-        if word in values:
-            sentimentNumber = values[word]
-            value = value + sentimentNumber
+    return sentiment_data
 
-    print("El siguiente tweet : " + tweet + " Tiene un sentimiento asociado de :" + str(value))
+def load_word_values(sentiment_records):
+    values = {}
+    for record in sentiment_records:
+        sentiment, value = record.split("\t")
+        values[sentiment] = int(value)
 
+    return values
 
+def generate_html(df, page_name):
+    text_file = open(page_name, "w")
+    text_file.write(df.to_html())
+    text_file.close()
+
+values = load_word_values(sentiment_records)
+tweets = get_tweets_text(tweet_records_file)
+sentiment_data = calculate_tweets_value(tweets, values)
+
+#Creando el dataframe para visualizar el sentimiento asociado
+sentiment_df = pd.DataFrame(sentiment_data, columns = ['Tweet','Sentimiento Asociado'])
+
+html = sentiment_df.sort_values('Sentimiento Asociado', ascending=False)
+
+generate_html(html, "index.html")
+
+#Creando el dataframe para ver el agrupamiento por valor de sentimiento asociado
+tweet_group = sentiment_df.groupby('Sentimiento Asociado').size().reset_index(name='Cantidad')
+
+generate_html(tweet_group,"group.html")
+
+#Generando el grafico de barras para el sentimiento asociado
+fig = tweet_group.plot.bar(x="Sentimiento Asociado", y="Cantidad")
+
+fig.figure.savefig('tweets_grouped_by_sentiment.pdf')
 
